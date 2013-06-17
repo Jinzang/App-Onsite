a13df385c1220236cc5ba4ed8db456ab17073325
Bernie Simon <bernie.simon@gmail.com>
2013-06-17 13:09:13 -0400

    New data type, directory to hold photos

---
+++ lib/App/Onsite/GalleryData.pm
@@ -0,0 +1,63 @@
+use strict;
+use warnings;
+use integer;
+
+#----------------------------------------------------------------------
+# A directory that contains a photo gallery
+
+package App::Onsite::GalleryData;
+
+use base qw(App::Onsite::DirData);
+
+#----------------------------------------------------------------------
+# Set default values
+
+sub parameters {
+    my ($pkg) = @_;
+
+    my %parameters = (
+	);
+
+    my %base_params = $pkg->SUPER::parameters();
+    %parameters = (%base_params, %parameters);
+
+    return %parameters;
+}
+
+#----------------------------------------------------------------------
+# Retrieve all records
+
+sub browse_data {
+    my ($self, $parentid, $limit) = @_;
+
+    return $self->App::Onsite::FileData::browse_data($parentid, $limit);
+}
+
+#----------------------------------------------------------------------
+# Return the names of the subdata objects contained in the file
+
+sub get_subtypes {
+    my ($self, $id) = @_;
+
+    $self->App::Onsite::PageData::get_subtypes($id);
+}
+
+#----------------------------------------------------------------------
+# Return true if there is only one subtype
+
+sub has_one_subtype {
+    my ($self, $id) = @_;
+
+    return $self->App::Onsite::PageData::has_one_subtype($id);
+}
+
+#----------------------------------------------------------------------
+# Get records whose contents match the hash
+
+sub search_data {
+    my ($self, $query, $parentid, $limit) = @_;
+
+    return $self->App::Onsite::FileData::search_data($query, $parentid, $limit);
+}
+
+1;
